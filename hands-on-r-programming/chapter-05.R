95ab21b58aa848c638fb5c7eacef2145153bcd56
hbunyamin <hendra.bunyamin@it.maranatha.edu>
2024-05-23 18:37:43 +0700

    Delete chapter-04.R

---
+++ hands-on-r-programming/chapter-05.R
@@ -0,0 +1,10 @@
+# ===============================
+#  4 Project 2: Playing Cards
+# ===============================
+
+# Task 1: build the deck
+# Task 2: write functions that deal and shuffle
+# Task 3: change the point system to suit your game
+# Task 4: manage the state of the deck
+
+
