41bfb802d027733e52202fdea1762d48cf145315
hbunyamin <hendra.bunyamin@it.maranatha.edu>
2024-08-11 10:56:42 +0700

    First commit

--- chapter-05.R
+++ chapter-05.R
@@ -18,7 +18,6 @@
 die <- c(1, 2, 3, 4, 5, 6)
 die
 
-
 # is.vector tests whether an object is an atomic vector. 
 # It returns TRUE if the object is an atomic vector and FALSE otherwise. 
 is.vector(die)
@@ -86,3 +85,65 @@ logic <- c(TRUE, FALSE, TRUE)
 logic
 
 typeof(logic)
+
+
+# ------------------------------------
+# Exercise 5.2 (Vector of Cards)
+# ------------------------------------
+hand <- c("ace", "king", "queen", "jack", "ten")
+hand
+
+typeof(hand)
+
+attributes(hand)
+
+# -------------------
+#   5.2.1 Names
+# -------------------
+names(die)
+
+names(die) <- c("one", "two", "three", "four", "five", "six")
+
+# Now die has a names attribute:
+names(die)
+attributes(die)
+die + 1
+
+# -------------------
+#   5.2.2 Dim
+# -------------------
+dim(die) <- c(2,3)
+die
+
+dim(die) <- c(3,2)
+die
+
+dim(die) <- c(1,2,3)
+die
+
+# ---------------------------------------------------------------
+#   5.3 Matrices
+#   - `matrix` will fill up the matrix column by column by default, 
+# ---------------------------------------------------------------
+m <- matrix(die, nrow = 2)
+m
+m <- matrix(die, nrow = 2, byrow = TRUE)
+m
+
+
+# ---------------------------------------------------------------------------------------------------------------
+#   5.4 Arrays
+#   - The array function creates an n-dimensional array.
+#   - array is not as customizeable as matrix and basically does the same thing as setting the dim attribute. 
+#   - To use array, provide an atomic vector as the first argument, and 
+#     a vector of dimensions as the second argument, now called dim
+# ----------------------------------------------------------------------------------------------------------------
+ar <- array(c(11:14, 21:24, 31:34), dim = c(2, 2, 3))
+ar
+
+
+
+
+
+
+
