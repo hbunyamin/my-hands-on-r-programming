45e8b2fecf6fa29c5c9120689e05b3fa05b52c7e
hbunyamin <hendra.bunyamin@it.maranatha.edu>
2024-08-18 13:14:39 +0700

    Masih di bab 7

--- chapter-07.R
+++ chapter-07.R
@@ -102,6 +102,28 @@ deck4[queenOfSpades,]
 # ---------------------------------------
 #   Exercise 7.3 (Practice with Tests)
 # ---------------------------------------
+# Pertanyaannya:
+#   Is w positive?
+#   Is x greater than 10 and less than 20?
+#   Is object y the word February?
+#   Is every value in z a day of the week? 
+w <- c(-1, 0, 1)
+x <- c(5, 15)
+y <- "February"
+z <- c("Monday", "Tuesday", "Friday")
+
+#   Is w positive?
+w > 0
+
+#   Is x greater than 10 and less than 20?
+x > 10 & x < 20
+
+#   Is object y the word February?
+y == "February"
 
 
+all(z %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
+
+# =============================================================
+deck5 <- deck
 
