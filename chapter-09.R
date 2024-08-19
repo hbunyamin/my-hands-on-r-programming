49d0a880ee6b973db896d18ab0951e299560a2d1
hbunyamin <hendra.bunyamin@it.maranatha.edu>
2024-08-19 18:24:28 +0700

    Mulai belajar Bab 9

---
+++ chapter-09.R
@@ -0,0 +1,7 @@
+########################
+# Bab 9 Programs
+########################
+
+# The play function will need to do two things. 
+# First, it will need to randomly generate three symbols; and, 
+# second, it will need to calculate a prize based on those symbols.
